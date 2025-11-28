b5fdd024d28dd93eea27f68e5cc584b0c3a26248
hasslhoph <hasslhoph@gmail.com>
2025-11-28 16:23:33 +0700

    Отправил файлы к лекции 3

---
+++ Lesson_3/Product.java
@@ -0,0 +1,27 @@
+public class Product {
+    public String name;
+    public String productionDate;
+    public String manufacturer;
+    public String countryOfOrigin;
+    public double price;
+    public boolean isReserved;
+
+    public Product(String name, String productionDate, String manufacturer,
+                   String countryOfOrigin, double price, boolean isReserved) {
+        this.name = name;
+        this.productionDate = productionDate;
+        this.manufacturer = manufacturer;
+        this.countryOfOrigin = countryOfOrigin;
+        this.price = price;
+        this.isReserved = isReserved;
+    }
+
+    public void printInfo() {
+        System.out.println("Название: " + name);
+        System.out.println("Дата производства: " + productionDate);
+        System.out.println("Производитель: " + manufacturer);
+        System.out.println("Страна происхождения: " + countryOfOrigin);
+        System.out.println("Цена: " + price);
+        System.out.println("Забронирован: " + isReserved);
+    }
+}
